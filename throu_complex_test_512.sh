d5814588b2459a7ceee96aa686db0ab380fcb5f4
root <root@node1.safetimer-u14.postman-pg0.wisc.cloudlab.us>
2018-01-04 14:58:03 -0700

    add throu_complex_test_*.sh

---
+++ throu_complex_test_512.sh
@@ -0,0 +1,39 @@
+pkt_size=$1
+dir=$2
+
+if [ "$#" -ne 2 ]
+then
+    echo "wrong parameters"
+    exit 1
+fi
+
+ssh 10.10.1.1 "killall receiver_app"
+rmmod hb_sender_tracker.ko
+killall sender_app
+killall sender_app_nb
+
+for clients in 300 400 500 600 700
+do
+    for repeat in 0 1 2 3 4
+    do
+        echo "idle clients $clients repeat $repeat"
+        ./test.sh $clients $pkt_size ./$dir/idle/$clients/$pkt_size $repeat
+        sleep 5
+        
+        echo "safetimer clients $clients repeat $repeat"
+        ssh 10.10.1.1 "/root/hb-latency/heartbeat/sbin/add_filter_rule.sh"
+        sleep 2
+        ssh 10.10.1.1 "/root/hb-latency/heartbeat/build/receiver_app > /dev/null" &
+        sleep 2
+        insmod /root/hb-latency/heartbeat/kmodule/send/hb_sender_tracker/hb_sender_tracker.ko
+        sleep 2
+        /root/hb-latency/heartbeat/build/sender_app &
+        ./test.sh $clients $pkt_size ./$dir/safetimer/$clients/$pkt_size $repeat
+        killall sender_app
+        rmmod hb_sender_tracker.ko
+        sleep 2
+        ssh 10.10.1.1 "killall receiver_app"
+        ssh 10.10.1.1 "/root/hb-latency/heartbeat/sbin/del_filter_rule.sh"
+        sleep 5
+    done
+done
--- throu_mkdir.sh
+++ throu_mkdir.sh
@@ -7,7 +7,7 @@ for i in 300 400 500
 do
     mkdir $dir/idle/$i
     mkdir $dir/safetimer/$i
-    for pkt_size in 128 256 512 1024
+    for pkt_size in 64 512 4096 65536
     do
         mkdir $dir/idle/$i/$pkt_size
         mkdir $dir/safetimer/$i/$pkt_size
